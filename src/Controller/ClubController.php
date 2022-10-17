ad53d5573034efed90515b1ceef44dcd2f6b0071
Oumaima-Jlidi <jlidi.oumaima@esprit.tn>
2022-10-17 01:06:03 +0100

    crud Student

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -30,9 +30,9 @@ class ClubController extends AbstractController
         $formations = array(
             array('ref' => 'form147', 'Titre' => 'Formation Symfony
             4','Description'=>'formation pratique',
-            'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020',
+            'date_debut'=>'12/06/2022', 'date_fin'=>'19/06/2020',
             'nb_participants'=>19) ,
-            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+            array('ref'=>'form1778','Titre'=>'Formation SOA' ,
             'Description'=>'formation theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
             'nb_participants'=>0),
             array('ref'=>'form178','Titre'=>'Formation Angular' ,
--- src/Controller/ClubStudentController.php
+++ src/Controller/ClubStudentController.php
@@ -21,15 +21,16 @@ class ClubStudentController extends AbstractController
         ]);
     }
     #[Route('/ajoutClubStudent', name: 'app_ajout_club')]
-    public function Ajoutstudent(Request $request, ManagerRegistry $doctrine):Response{
+    public function Ajoutstudent(Request $request, ManagerRegistry $doctrine): Response
+    {
         $club=new Club();
         $form = $this->createForm(ClubType::class, $club);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-        $em=$doctrine->getManager();
-        $em->persist($club);
-        $em->flush();
+            $em=$doctrine->getManager();
+            $em->persist($club);
+            $em->flush();
             return $this->redirectToRoute('app_club_student', [], Response::HTTP_SEE_OTHER);
         }
 
